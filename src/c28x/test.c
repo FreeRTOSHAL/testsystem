c6b272512f06d1637ae9fb465cda4eb7a352acbb
Andreas Werner <kernel@andy89.org>
2020-11-30 14:57:42 +0100

    add main

---
+++ src/c28x/test.c
@@ -0,0 +1,45 @@
+/* SPDX-License-Identifier: MIT */
+/*
+ * Author: Andreas Werner <kernel@andy89.org>
+ * Date: 2016
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <FreeRTOS.h>
+#include <task.h>
+# define PRINTF(...) printf(__VA_ARGS__)
+
+void vApplicationMallocFailedHook( void ) {
+	CONFIG_ASSERT(0);
+}
+
+void vApplicationTickHook() {
+
+}
+
+void vApplicationStackOverflowHook( TaskHandle_t xTask, char *pcTaskName ) {
+	CONFIG_ASSERT(0);
+}
+
+void vApplicationIdleHook( void ) {
+}
+
+void userErrorHandler(void) {
+
+}
+
+void vApplicationSetupTimerInterrupt(void) {
+
+}
+
+int main() {
+	int32_t ret;
+	//ret = irq_init();
+	//CONFIG_ASSERT(ret == 0);
+	PRINTF("Start Scheduler\n");
+	vTaskStartScheduler ();
+	for(;;);
+	return 0;
+}
+
