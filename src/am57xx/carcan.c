c8d8e9ff2c172371f8bb111f7be62e246af027c4
Theodor Bauer <tbaue001@ad.local.cs.hs-rm.de>
2020-12-12 09:05:43 +0100

    changed tege tests

--- src/am57xx/carcan.c
+++ src/am57xx/carcan.c
@@ -4,9 +4,19 @@
 #include <task.h>
 #include <os.h>
 #include <string.h>
+#include <stdio.h>
 
 void can_test() {
-    struct can *can = can_init(CARCAN1_ID, 500000, NULL, false, false, NULL);
-    can_deinit(can);
+    struct can *can1 = can_init(CARCAN1_ID, 500000, NULL, false, false, NULL);
+    printf("start can1 can_send\n");
+    struct can_msg msg = {0, 2, 0, 8, {4,20, 69, 66, 21, 100, 13, 37}};
+    can_send(can1, &msg, 1000);
+    can_deinit(can1);
+
+
+    struct can *can2 = can_init(CARCAN2_ID, 500000, NULL, false, false, NULL);
+    printf("start can2 can_send\n");
+    can_send(can2, &msg, 1000);
+    can_deinit(can2);
 }
 
