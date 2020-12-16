94c148f1300b0269387865e5edbfd894c34c7948
Theodor Bauer <tbaue001@ad.local.cs.hs-rm.de>
2020-12-16 16:57:31 +0100

    changed can test

--- src/am57xx/carcan.c
+++ src/am57xx/carcan.c
@@ -7,16 +7,24 @@
 #include <stdio.h>
 
 void can_test() {
-    struct can *can1 = can_init(CARCAN1_ID, 500000, NULL, false, false, NULL);
+    struct can *can1;
+    struct can_msg msg_send = {0, 2, 0, 8, {4,20, 69, 66, 21, 100, 13, 37}};
+    struct can_msg msg_recv;
+    struct can_filter filter = {2, 0};
+    int filterID;
+    can1 = can_init(CARCAN1_ID, 500000, NULL, false, false, NULL);
     printf("start can1 can_send\n");
-    struct can_msg msg = {0, 2, 0, 8, {4,20, 69, 66, 21, 100, 13, 37}};
-    can_send(can1, &msg, 1000);
+    filterID = can_registerFilter(can1, &filter);
+    can_send(can1, &msg_send, 10000);
+    can_recv(can1,filterID, &msg_recv, 10000);
+    can_deregisterFilter(can1, filterID);
     can_deinit(can1);
 
-
+    /*
     struct can *can2 = can_init(CARCAN2_ID, 500000, NULL, false, false, NULL);
     printf("start can2 can_send\n");
     can_send(can2, &msg, 1000);
     can_deinit(can2);
+    */
 }
 
