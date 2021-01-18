15999c4d537f54284ba477ca16da5341822e00e8
Theodor Bauer <tbaue001@ad.local.cs.hs-rm.de>
2021-01-18 08:36:02 +0100

    dcan test now in a task

--- src/am57xx/dcan.c
+++ src/am57xx/dcan.c
@@ -5,26 +5,87 @@
 #include <os.h>
 #include <string.h>
 #include <stdio.h>
+#include <iomux.h>
+#include <gpio.h>
 
-void can_test() {
-    struct can *can1;
-    struct can_msg msg_send = {0, 2, 0, 8, {4,20, 69, 66, 21, 100, 13, 37}};
-    struct can_msg msg_recv;
-    struct can_filter filter = {2, 0};
-    int filterID;
-    can1 = can_init(DCAN1_ID, 500000, NULL, false, false, NULL);
-    printf("start can1 can_send\n");
-    filterID = can_registerFilter(can1, &filter);
-    can_send(can1, &msg_send, 10000);
-    can_recv(can1,filterID, &msg_recv, 10000);
-    can_deregisterFilter(can1, filterID);
-    can_deinit(can1);
+OS_DEFINE_TASK(taskCAN, 512);
 
-    /*
-    struct can *can2 = can_init(DCAN2_ID, 500000, NULL, false, false, NULL);
-    printf("start can2 can_send\n");
-    can_send(can2, &msg, 1000);
-    can_deinit(can2);
-    */
+void dcan1_tx_high(){
+    int i;
+    struct gpio *gpio = gpio_init(GPIO_ID);
+    struct gpio_pin *cantx = gpioPin_init(gpio,PAD_DCAN1_TX, GPIO_OUTPUT, GPIO_PULL_DOWN);
+    CONFIG_ASSERT(cantx);
+    gpioPin_clearPin(cantx);
+    gpioPin_setPin(cantx);
+
 }
 
+
+void can_task(void *data) {
+    struct can *can1 = data;
+    struct can_msg msg_send = {0, 2, 0, 8, {4,20, 69, 66, 21, 100, 13, 37}};
+    struct can_msg msg_recv;
+    struct can_filter filter = {3, 0};
+    int filterID, i;
+    int32_t ret;
+    memset(&msg_recv, 0xFF, sizeof(struct can_msg));
+    //filterID = can_registerFilter(can1, &filter);
+    //CONFIG_ASSERT(filterID >= 0);
+    //ret = can_send(can1, &msg_send, (1000 / portTICK_PERIOD_MS));
+    //CONFIG_ASSERT(ret == 0);
+
+    /*
+       ret = can_recv(can1,filterID, &msg_recv, (1000 / portTICK_PERIOD_MS));
+       CONFIG_ASSERT(ret == 0);
+       printf("can_test: received msg\nts: %i\nid: %i\nreq: %i\nlength: %i\n", msg_recv.ts, msg_recv.id, msg_recv.req, msg_recv.length);
+       for(i = 0; i < msg_recv.length; ++i){
+       printf("data[%i]: %i\n", i, msg_recv.data[i]);
+       }
+       */
+
+
+
+    /*
+    for(;;){
+        ret = can_send(can1, &msg_send, (1000 / portTICK_PERIOD_MS));
+        CONFIG_ASSERT(ret == 0);
+        ret = can_recv(can1,filterID, &msg_recv, (10000 / portTICK_PERIOD_MS));
+        CONFIG_ASSERT(ret == 0);
+        CONFIG_ASSERT(msg_recv.id == msg_send.id);
+        CONFIG_ASSERT(msg_recv.length == msg_send.length);
+        //CONFIG_ASSERT(msg_recv.req == msg_send.req);
+        for (i = 0; i < 8; i++) {
+            CONFIG_ASSERT(msg_recv.data[i] == msg_send.data[i]);
+        }
+
+
+    }
+    */
+
+    //can_deregisterFilter(can1, filterID);
+    //can_deinit(can1);
+    /*
+
+    for(;;){
+        ret = can_recv(can1, filterID, &msg_recv, (1000 / portTICK_PERIOD_MS));
+        if(ret == 0){
+            printf("can_test: received msg\nts: %i\nid: %i\nreq: %i\nlength: %i\n", msg_recv.ts, msg_recv.id, msg_recv.req, msg_recv.length);
+            for(i = 0; i < msg_recv.length; ++i){
+                printf("data[%i]: %i\n", i, msg_recv.data[i]);
+            }
+        }
+        else {
+            printf("can_test: received nothing\n");
+        }
+    }
+    */
+    printf("can_test finished\n\n");
+    for(;;);
+}
+
+void can_test() {
+    struct can *can = can_init(DCAN1_ID, 500000, NULL, false, false, NULL);
+    OS_CREATE_TASK(can_task, "CAN Test", 512, can, 2, taskCAN);
+    //dcan1_tx_high();
+
+}
