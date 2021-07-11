89572f4dfab84e7678ae3ab6812fd92fa3c7b03d
Andreas Werner <kernel@andy89.org>
2021-07-11 14:20:13 +0200

    add 64 Bit support

--- qemu.sh
+++ qemu.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 #qemu-system-riscv32 $* -nographic -machine virt -net none -chardev stdio,id=con,mux=on -serial chardev:con -mon chardev=con,mode=readline -bios none -smp 4 -kernel test
-qemu-system-riscv32 $* -nographic -machine virt -net none -chardev stdio,id=con,mux=on -serial chardev:con -mon chardev=con,mode=readline -smp 4 -bios ~/boards/riscv/opensbi/install/share/opensbi/ilp32/generic/firmware/fw_dynamic.elf -kernel test
+#qemu-system-riscv32 $* -nographic -machine virt -net none -chardev stdio,id=con,mux=on -serial chardev:con -mon chardev=con,mode=readline -smp 4 -bios ~/boards/riscv/opensbi/install/share/opensbi/ilp32/generic/firmware/fw_dynamic.elf -kernel test
+#qemu-system-riscv64 $* -nographic -machine virt -net none -chardev stdio,id=con,mux=on -serial chardev:con -mon chardev=con,mode=readline -bios none -smp 4 -kernel test
+qemu-system-riscv64 $* -nographic -machine virt -net none -chardev stdio,id=con,mux=on -serial chardev:con -mon chardev=con,mode=readline -smp 4 -kernel test
