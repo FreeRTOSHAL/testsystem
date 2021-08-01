5fe9779a1fa55309c26c413608bc4ec956e41f7d
Andreas Werner <kernel@andy89.org>
2021-08-01 18:20:35 +0200

    polarfire: add PolarFire Tests

---
+++ include/polarfire/floattest.h
@@ -0,0 +1,4 @@
+#ifndef FLOAT_TEST_H_
+#define FLOAT_TEST_H_
+void floattest_init(void);
+#endif
---
+++ src/polarfire/test.c
@@ -0,0 +1,105 @@
+/* SPDX-License-Identifier: MIT */
+/*
+ * Author: Andreas Werner <kernel@andy89.org>
+ * Date: 2016
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <FreeRTOS.h>
+#include <task.h>
+#include <gpio.h>
+#include <devs.h>
+#include <uart.h>
+#include <newlib_stub.h>
+#include <nlibc_stub.h>
+#include <irq.h>
+#include <timer.h>
+#include <uarttest.h>
+#include <timertest.h>
+#include <floattest.h>
+#include <sbi_uart.h>
+#if defined(CONFIG_NEWLIB) || defined(CONFIG_NLIBC_PRINTF)
+# define PRINTF(...) printf(__VA_ARGS__)
+#else
+# define PRINTF(...) 
+#endif
+
+void vApplicationMallocFailedHook( void ) {
+	CONFIG_ASSERT(0);
+}
+
+void vApplicationTickHook() {
+
+}
+
+void vApplicationStackOverflowHook( TaskHandle_t xTask, char *pcTaskName ) {
+	CONFIG_ASSERT(0);
+}
+
+void vApplicationIdleHook( void ) {
+}
+
+
+#ifdef CONFIG_USE_STATS_FORMATTING_FUNCTIONS
+void taskManTask(void *data) {
+	TickType_t lastWakeUpTime = xTaskGetTickCount();
+	static char taskBuff[5 * 1024];
+	for(;;) {
+		vTaskList(taskBuff);
+		PRINTF("name\t\tState\tPrio\tStack\tTaskNr.\n");
+		PRINTF("%s", taskBuff);
+		PRINTF("blocked ('B'), ready ('R'), deleted ('D') or suspended ('S')\n");
+#ifdef CONFIG_GENERATE_RUN_TIME_STATS
+		PRINTF("name\t\tTime\t\t%%\n");
+		vTaskGetRunTimeStats(taskBuff);
+		PRINTF("%s", taskBuff);
+#endif
+		PRINTF("\n");
+		vTaskDelayUntil(&lastWakeUpTime, 1000 / portTICK_PERIOD_MS);
+	}
+}
+#endif
+
+OS_DEFINE_TASK(taskLED, 128);
+OS_DEFINE_TASK(taskMan, 512);
+int main() {
+	int32_t ret;
+	ret = irq_init();
+	CONFIG_ASSERT(ret == 0);
+#ifdef CONFIG_UART
+# ifdef CONFIG_POLAFIRE_SBI_STDOUT_UART
+	struct uart *uart = uart_init(SBI_UART_ID, 115200);
+# endif
+	CONFIG_ASSERT(uart != NULL);
+# ifdef CONFIG_NEWLIB
+	ret = newlib_init(uart, uart);
+	CONFIG_ASSERT(ret == 0);
+# endif
+# ifdef CONFIG_NLIBC_PRINTF
+	ret = nlibc_init(uart, uart);
+	CONFIG_ASSERT(ret == 0);
+# endif
+#endif
+	PRINTF("Init Devices\n");
+#ifdef CONFIG_INSTANCE_NAME
+	hal_printNames();
+#endif
+#ifdef CONFIG_USE_STATS_FORMATTING_FUNCTIONS
+	OS_CREATE_TASK(taskManTask, "Task Manager Task", 1024, NULL, 1, taskMan);
+#endif
+#ifdef CONFIG_UART_TEST
+	uartTest_init();
+#endif
+#ifdef CONFIG_TIMER_TEST
+	timertest_init();
+#endif
+#ifdef CONFIG_FLOAT_TEST
+	floattest_init();
+#endif
+	PRINTF("Running on Hart ID: %u\n", irq_getCPUID());
+	PRINTF("Start Scheduler\n");
+	vTaskStartScheduler ();
+	for(;;);
+	return 0;
+}
