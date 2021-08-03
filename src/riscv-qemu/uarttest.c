5366ca97cfb19d157e8050d442f87f85fa428bb3
Andreas Werner <kernel@andy89.org>
2021-08-03 11:26:23 +0200

    riscv: SBI UART Test

--- src/riscv-qemu/uarttest.c
+++ src/riscv-qemu/uarttest.c
@@ -1,5 +1,6 @@
 #include <uart.h>
 #include <devs.h>
+#include <sbi_uart.h>
 
 static void echoTask(void *data) {
 	struct uart *uart = data;
@@ -7,8 +8,8 @@ static void echoTask(void *data) {
 	int32_t ret;
 	uart_puts(uart, "Start Testing\n\r", portMAX_DELAY);
 	for (;;) {
-		//c = uart_getc(uart, portMAX_DELAY);
-		c = uart_getcISR(uart);
+		c = uart_getc(uart, portMAX_DELAY);
+		//c = uart_getcISR(uart);
 		if (c == '\n') {
 			ret = uart_putc(uart, '\r', portMAX_DELAY);
 			CONFIG_ASSERT(ret >= 0);
@@ -21,7 +22,11 @@ static void echoTask(void *data) {
 OS_DEFINE_TASK(echo, 512);
 void uartTest_init() {
 	BaseType_t bret;
+#ifdef CONFIG_UART_TEST_SBI
+	struct uart *uart = uart_init(SBI_UART_ID, 115200);
+#else
 	struct uart *uart = uart_init(UART0_ID, 115200);
+#endif
 	CONFIG_ASSERT(uart != NULL);
 	bret = OS_CREATE_TASK(echoTask, "echo", 512, uart, 2, echo);
 	CONFIG_ASSERT(bret == pdPASS);
